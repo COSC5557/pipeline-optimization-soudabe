16733be42de33fcf03e8e824bc4a0f1943e99ae7
soudabe <soudabe.bolouri@gmail.com>
2023-11-26 19:02:50 -0700

    Add more classifiers

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import time
+
+import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 from sklearn.compose import ColumnTransformer
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.impute import SimpleImputer
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split, RandomizedSearchCV
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.svm import SVC
@@ -98,24 +103,60 @@ if __name__ == "__main__":
     categorical_features = X.select_dtypes(include=['object']).columns
 
     preprocessor = preprocess_pipeline(numeric_features, categorical_features)
-    svm_pipeline = create_pipeline(preprocessor, SVC())
 
-    simple_accuracy, simple_time = evaluate_pipeline(svm_pipeline, X_train, y_train, X_test, y_test)
-
-    param_grid = {
-        'classifier__C': [0.001, 0.01, 10, 100],
-        'classifier__gamma': [0.1, 0.01, 0.001, 0.0001],
-        'classifier__kernel': ['linear', 'rbf', 'poly']
+    classifiers = {
+        'SVM': (SVC(), 'SVM', {
+            'classifier__C': [0.001, 0.01, 10, 100],
+            'classifier__gamma': [0.1, 0.01, 0.001, 0.0001],
+            'classifier__kernel': ['linear', 'rbf', 'poly']
+        }),
+        'Random Forest': (RandomForestClassifier(), 'RF', {
+            'classifier__n_estimators': [5,20,50,100],
+            'classifier__max_depth': [int(x) for x in np.linspace(10, 120, num = 12)],
+            'classifier__max_features': ['auto', 'sqrt'],
+            'classifier__min_samples_leaf': [1, 3, 4],
+            'classifier__min_samples_split': [2, 6, 10],
+            'classifier__bootstrap': [True, False],
+        }),
+        'Logistic Regression': (LogisticRegression(), 'LR', {
+            'classifier__penalty': ['l1', 'l2', 'elasticnet', 'none'],
+            'classifier__C': np.logspace(-4, 4, 20),
+            'classifier__solver': ['lbfgs', 'newton-cg', 'liblinear', 'sag', 'saga'],
+            'classifier__max_iter': [100, 1000, 2500, 5000]
+        }),
+        'K-Nearest Neighbors': (KNeighborsClassifier(), 'KNN', {
+            'classifier__n_neighbors': np.arange(2, 30, 1),
+            'classifier__weights': ['uniform', 'distance']
+        })
     }
 
-    best_params, optimized_accuracy, optimized_time = hyperparameter_tuning(svm_pipeline, param_grid, X_train, y_train,
-                                                                            X_test, y_test)
+    results = {}
 
-    print(f"Time for fitting the simple pipeline: {simple_time:.4f} seconds")
-    print(f"Accuracy without hyperparameter optimization: {simple_accuracy:.4f}")
-    print(f"Best Hyperparameters: {best_params}")
-    print(f"Time for fitting the pipeline with hyperparameter optimization: {optimized_time:.4f} seconds")
-    print(f"Accuracy with hyperparameter optimization: {optimized_accuracy:.4f}")
+    for clf_name, (classifier, label, param_grid) in classifiers.items():
+        pipeline = create_pipeline(preprocessor, classifier)
+        simple_accuracy, simple_time = evaluate_pipeline(pipeline, X_train, y_train, X_test, y_test)
+        best_params, optimized_accuracy, optimized_time = hyperparameter_tuning(pipeline, param_grid, X_train, y_train,
+                                                                                X_test, y_test)
+        results[clf_name] = {
+            'label': label,
+            'best_params': best_params,
+            'simple_accuracy': simple_accuracy,
+            'simple_time': simple_time,
+            'optimized_accuracy': optimized_accuracy,
+            'optimized_time': optimized_time
+        }
 
-    plot_comparison(['Simple Pipeline', 'Optimized Pipeline'], [simple_accuracy, optimized_accuracy],
-                    [simple_time, optimized_time])
+    for clf_name, result in results.items():
+        print(f"Classifier: {clf_name}")
+        print(f"Time for fitting the simple pipeline: {result['simple_time']:.4f} seconds")
+        print(f"Accuracy without hyperparameter optimization: {result['simple_accuracy']:.4f}")
+        print(f"Best Hyperparameters: {result['best_params']}")
+        print(f"Time for fitting the pipeline with hyperparameter optimization: {result['optimized_time']:.4f} seconds")
+        print(f"Accuracy with hyperparameter optimization: {result['optimized_accuracy']:.4f}")
+        print("---------------------------------------------------------")
+
+    labels = [result['label'] for result in results.values()]
+    accuracy_scores = [result['optimized_accuracy'] for result in results.values()]
+    training_times = [result['optimized_time'] for result in results.values()]
+
+    plot_comparison(labels, accuracy_scores, training_times)
