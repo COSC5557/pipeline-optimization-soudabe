1820fa96e6bf3c191e7d00dc23c83546ff2afb05
soudabe <soudabe.bolouri@gmail.com>
2023-12-11 22:57:15 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import time
-
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
@@ -7,7 +5,7 @@ from sklearn.compose import ColumnTransformer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.impute import SimpleImputer
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split, RandomizedSearchCV
+from sklearn.model_selection import train_test_split, RandomizedSearchCV, StratifiedKFold
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
@@ -24,7 +22,7 @@ def separate_features_target(df, target_column):
     return features, target
 
 
-def preprocess_pipeline(numeric_features, categorical_features):
+def preprocess_pipeline():
     numeric_transformer = Pipeline(steps=[
         ('imputer', SimpleImputer(strategy='median')),
         ('scaler', StandardScaler())
@@ -35,96 +33,75 @@ def preprocess_pipeline(numeric_features, categorical_features):
         ('onehot', OneHotEncoder(handle_unknown='ignore'))
     ])
 
-    preprocessor = ColumnTransformer(
+    return ColumnTransformer(
         transformers=[
             ('num', numeric_transformer, numeric_features),
             ('cat', categorical_transformer, categorical_features)
         ])
 
-    return preprocessor
+
+def nested_cross_validation(X, y, classifier, param_grid):
+    outer_cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+    inner_cv = StratifiedKFold(n_splits=3, shuffle=True, random_state=42)
+
+    nested_scores = []
+    for train_idx, test_idx in outer_cv.split(X, y):
+        X_train, X_test = X.iloc[train_idx], X.iloc[test_idx]
+        y_train, y_test = y.iloc[train_idx], y.iloc[test_idx]
+
+        pipeline = Pipeline([
+            ('preprocessor', preprocess_pipeline()),
+            ('classifier', classifier)
+        ])
+
+        random_search = RandomizedSearchCV(pipeline, param_distributions=param_grid, n_iter=10, cv=inner_cv, random_state=42)
+        random_search.fit(X_train, y_train)
+        best_model = random_search.best_estimator_
+
+        nested_score = best_model.score(X_test, y_test)
+        nested_scores.append(nested_score)
+
+    return np.mean(nested_scores)
 
 
-def create_pipeline(preprocessor, classifier):
-    return Pipeline([
-        ('preprocessor', preprocessor),
-        ('classifier', classifier)
-    ])
-
-
-def evaluate_pipeline(pipeline, X_train, y_train, X_test, y_test):
-    start_time = time.time()
-    pipeline.fit(X_train, y_train)
-    end_time = time.time()
-    accuracy = pipeline.score(X_test, y_test)
-    return accuracy, end_time - start_time
-
-
-def hyperparameter_tuning(pipeline, param_grid, X_train, y_train, X_test, y_test):
-    random_search = RandomizedSearchCV(pipeline, param_distributions=param_grid, n_iter=10, cv=5, random_state=42)
-
-    start_time = time.time()
-    random_search.fit(X_train, y_train)
-    end_time = time.time()
-
-    best_params = random_search.best_params_
-    accuracy = random_search.score(X_test, y_test)
-
-    return best_params, accuracy, end_time - start_time
-
-
-def plot_comparison(labels, accuracy_scores, training_times):
-    fig, ax1 = plt.subplots()
-
-    color = 'tab:blue'
-    ax1.set_xlabel('')
-    ax1.set_ylabel('Accuracy', color=color)
-    ax1.bar(labels, accuracy_scores, color=color)
-    ax1.tick_params(axis='y', labelcolor=color)
-
-    ax2 = ax1.twinx()
-    color = 'tab:red'
-    ax2.set_ylabel('Training Time (seconds)', color=color)
-    ax2.plot(labels, training_times, marker='o', linestyle='-', color=color)
-    ax2.tick_params(axis='y', labelcolor=color)
-
-    fig.tight_layout()
-    plt.title('Accuracy and Training Time Comparison')
+def plot_comparison(labels, nested_cv_scores):
+    plt.figure(figsize=(10, 6))
+    plt.bar(labels, nested_cv_scores, color='tab:blue')
+    plt.xlabel('Classifiers')
+    plt.ylabel('Nested CV Accuracy')
+    plt.title('Nested Cross-Validation Accuracy Comparison')
     plt.show()
 
 
 if __name__ == "__main__":
     data_frame = pd.read_csv("winequality-white.csv", sep=";")
-    train_set, test_set = split_data(data_frame)
     X = data_frame.drop('quality', axis=1)
-    X_train, y_train = separate_features_target(train_set, target_column="quality")
-    X_test, y_test = separate_features_target(test_set, target_column="quality")
+    y = data_frame['quality']
 
     numeric_features = X.select_dtypes(include=['int64', 'float64']).columns
     categorical_features = X.select_dtypes(include=['object']).columns
 
-    preprocessor = preprocess_pipeline(numeric_features, categorical_features)
-
     classifiers = {
-        'SVM': (SVC(), 'SVM', {
+        'SVM': (SVC(), {
             'classifier__C': [0.001, 0.01, 10, 100],
             'classifier__gamma': [0.1, 0.01, 0.001, 0.0001],
             'classifier__kernel': ['linear', 'rbf', 'poly']
         }),
-        'Random Forest': (RandomForestClassifier(), 'RF', {
-            'classifier__n_estimators': [5,20,50,100],
-            'classifier__max_depth': [int(x) for x in np.linspace(10, 120, num = 12)],
+        'Random Forest': (RandomForestClassifier(), {
+            'classifier__n_estimators': [5, 20, 50, 100],
+            'classifier__max_depth': [int(x) for x in np.linspace(10, 120, num=12)],
             'classifier__max_features': ['auto', 'sqrt'],
             'classifier__min_samples_leaf': [1, 3, 4],
             'classifier__min_samples_split': [2, 6, 10],
             'classifier__bootstrap': [True, False],
         }),
-        'Logistic Regression': (LogisticRegression(), 'LR', {
+        'Logistic Regression': (LogisticRegression(), {
             'classifier__penalty': ['l1', 'l2', 'elasticnet', 'none'],
             'classifier__C': np.logspace(-4, 4, 20),
             'classifier__solver': ['lbfgs', 'newton-cg', 'liblinear', 'sag', 'saga'],
             'classifier__max_iter': [100, 1000, 2500, 5000]
         }),
-        'K-Nearest Neighbors': (KNeighborsClassifier(), 'KNN', {
+        'K-Nearest Neighbors': (KNeighborsClassifier(), {
             'classifier__n_neighbors': np.arange(2, 30, 1),
             'classifier__weights': ['uniform', 'distance']
         })
@@ -132,31 +109,14 @@ if __name__ == "__main__":
 
     results = {}
 
-    for clf_name, (classifier, label, param_grid) in classifiers.items():
-        pipeline = create_pipeline(preprocessor, classifier)
-        simple_accuracy, simple_time = evaluate_pipeline(pipeline, X_train, y_train, X_test, y_test)
-        best_params, optimized_accuracy, optimized_time = hyperparameter_tuning(pipeline, param_grid, X_train, y_train,
-                                                                                X_test, y_test)
-        results[clf_name] = {
-            'label': label,
-            'best_params': best_params,
-            'simple_accuracy': simple_accuracy,
-            'simple_time': simple_time,
-            'optimized_accuracy': optimized_accuracy,
-            'optimized_time': optimized_time
-        }
-
-    for clf_name, result in results.items():
+    for clf_name, (classifier, param_grid) in classifiers.items():
+        nested_cv_score = nested_cross_validation(X, y, classifier, param_grid)
+        results[clf_name] = nested_cv_score
         print(f"Classifier: {clf_name}")
-        print(f"Time for fitting the simple pipeline: {result['simple_time']:.4f} seconds")
-        print(f"Accuracy without hyperparameter optimization: {result['simple_accuracy']:.4f}")
-        print(f"Best Hyperparameters: {result['best_params']}")
-        print(f"Time for fitting the pipeline with hyperparameter optimization: {result['optimized_time']:.4f} seconds")
-        print(f"Accuracy with hyperparameter optimization: {result['optimized_accuracy']:.4f}")
+        print(f"Nested CV Accuracy: {nested_cv_score:.4f}")
         print("---------------------------------------------------------")
 
-    labels = [result['label'] for result in results.values()]
-    accuracy_scores = [result['optimized_accuracy'] for result in results.values()]
-    training_times = [result['optimized_time'] for result in results.values()]
+    labels = list(results.keys())
+    nested_cv_scores = list(results.values())
 
-    plot_comparison(labels, accuracy_scores, training_times)
+    plot_comparison(labels, nested_cv_scores)
