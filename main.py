b289c39ff77be8ab183d86fd43286d27350b9f06
soudabe <soudabe.bolouri@gmail.com>
2023-12-12 22:12:22 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -4,16 +4,11 @@ from matplotlib import pyplot as plt
 from sklearn.compose import ColumnTransformer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.impute import SimpleImputer
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split, RandomizedSearchCV, StratifiedKFold
+from sklearn.model_selection import RandomizedSearchCV, StratifiedKFold, cross_val_score
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.svm import SVC
-
-
-def split_data(df, test_size=0.2, random_state=42):
-    return train_test_split(df, test_size=test_size, random_state=random_state)
 
 
 def separate_features_target(df, target_column):
@@ -22,14 +17,14 @@ def separate_features_target(df, target_column):
     return features, target
 
 
-def preprocess_pipeline():
+def preprocess_pipeline(numeric_strategy, categorical_strategy):
     numeric_transformer = Pipeline(steps=[
-        ('imputer', SimpleImputer(strategy='median')),
+        ('imputer', SimpleImputer(strategy=numeric_strategy)),
         ('scaler', StandardScaler())
     ])
 
     categorical_transformer = Pipeline(steps=[
-        ('imputer', SimpleImputer(strategy='most_frequent')),
+        ('imputer', SimpleImputer(strategy=categorical_strategy)),
         ('onehot', OneHotEncoder(handle_unknown='ignore'))
     ])
 
@@ -49,12 +44,7 @@ def nested_cross_validation(X, y, classifier, param_grid):
         X_train, X_test = X.iloc[train_idx], X.iloc[test_idx]
         y_train, y_test = y.iloc[train_idx], y.iloc[test_idx]
 
-        pipeline = Pipeline([
-            ('preprocessor', preprocess_pipeline()),
-            ('classifier', classifier)
-        ])
-
-        random_search = RandomizedSearchCV(pipeline, param_distributions=param_grid, n_iter=10, cv=inner_cv, random_state=42)
+        random_search = RandomizedSearchCV(classifier, param_distributions=param_grid, n_iter=10, cv=inner_cv, random_state=42)
         random_search.fit(X_train, y_train)
         best_model = random_search.best_estimator_
 
@@ -64,13 +54,10 @@ def nested_cross_validation(X, y, classifier, param_grid):
     return np.mean(nested_scores)
 
 
-def plot_comparison(labels, nested_cv_scores):
-    plt.figure(figsize=(10, 6))
-    plt.bar(labels, nested_cv_scores, color='tab:blue')
-    plt.xlabel('Classifiers')
-    plt.ylabel('Nested CV Accuracy')
-    plt.title('Nested Cross-Validation Accuracy Comparison')
-    plt.show()
+def evaluate_default_performance(X, y, classifier):
+    outer_cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+    scores = cross_val_score(classifier, X, y, cv=outer_cv)
+    return np.mean(scores)
 
 
 if __name__ == "__main__":
@@ -82,41 +69,81 @@ if __name__ == "__main__":
     categorical_features = X.select_dtypes(include=['object']).columns
 
     classifiers = {
-        'SVM': (SVC(), {
-            'classifier__C': [0.001, 0.01, 10, 100],
-            'classifier__gamma': [0.1, 0.01, 0.001, 0.0001],
-            'classifier__kernel': ['linear', 'rbf', 'poly']
-        }),
-        'Random Forest': (RandomForestClassifier(), {
+        'SVM': Pipeline([
+            ('preprocessor', preprocess_pipeline('median', 'most_frequent')),
+            ('classifier', SVC())
+        ]),
+        'Random Forest': Pipeline([
+            ('preprocessor', preprocess_pipeline('median', 'most_frequent')),
+            ('classifier', RandomForestClassifier())
+        ]),
+        'K-Nearest Neighbors': Pipeline([
+            ('preprocessor', preprocess_pipeline('median', 'most_frequent')),
+            ('classifier', KNeighborsClassifier())
+        ])
+    }
+
+    param_grids = {
+        'SVM': {
+            'preprocessor__num__imputer__strategy': ['median'],
+            'preprocessor__cat__imputer__strategy': ['most_frequent'],
+            'classifier__C': [1, 10, 100],
+            'classifier__gamma': [0.01, 0.1, 'scale'],
+            'classifier__kernel': ['rbf']
+        },
+        'Random Forest': {
+            'preprocessor__num__imputer__strategy': ['mean', 'median'],
+            'preprocessor__cat__imputer__strategy': ['most_frequent', 'constant'],
             'classifier__n_estimators': [5, 20, 50, 100],
             'classifier__max_depth': [int(x) for x in np.linspace(10, 120, num=12)],
             'classifier__max_features': ['auto', 'sqrt'],
             'classifier__min_samples_leaf': [1, 3, 4],
             'classifier__min_samples_split': [2, 6, 10],
             'classifier__bootstrap': [True, False],
-        }),
-        'Logistic Regression': (LogisticRegression(), {
-            'classifier__penalty': ['l1', 'l2', 'elasticnet', 'none'],
-            'classifier__C': np.logspace(-4, 4, 20),
-            'classifier__solver': ['lbfgs', 'newton-cg', 'liblinear', 'sag', 'saga'],
-            'classifier__max_iter': [100, 1000, 2500, 5000]
-        }),
-        'K-Nearest Neighbors': (KNeighborsClassifier(), {
+        },
+        'K-Nearest Neighbors': {
+            'preprocessor__num__imputer__strategy': ['mean', 'median'],
+            'preprocessor__cat__imputer__strategy': ['most_frequent', 'constant'],
             'classifier__n_neighbors': np.arange(2, 30, 1),
             'classifier__weights': ['uniform', 'distance']
-        })
+        }
     }
 
-    results = {}
+    results_default = {}
+    results_tuned = {}
 
-    for clf_name, (classifier, param_grid) in classifiers.items():
-        nested_cv_score = nested_cross_validation(X, y, classifier, param_grid)
-        results[clf_name] = nested_cv_score
+    for clf_name in classifiers.keys():
+        classifier = classifiers[clf_name]
+
+        default_score = evaluate_default_performance(X, y, classifier)
+        results_default[clf_name] = default_score
+
+        param_grid = param_grids[clf_name]
+        tuned_score = nested_cross_validation(X, y, classifier, param_grid)
+        results_tuned[clf_name] = tuned_score
+
         print(f"Classifier: {clf_name}")
-        print(f"Nested CV Accuracy: {nested_cv_score:.4f}")
+        print(f"Default Accuracy: {default_score:.4f}")
+        print(f"Tuned Accuracy: {tuned_score:.4f}")
         print("---------------------------------------------------------")
 
-    labels = list(results.keys())
-    nested_cv_scores = list(results.values())
+    labels = list(results_default.keys())
+    default_scores = list(results_default.values())
+    tuned_scores = list(results_tuned.values())
 
-    plot_comparison(labels, nested_cv_scores)
+    x = np.arange(len(labels))
+    width = 0.35
+
+    fig, ax = plt.subplots(figsize=(12, 6))
+    rects1 = ax.bar(x - width / 2, default_scores, width, label='Default Accuracy')
+    rects2 = ax.bar(x + width / 2, tuned_scores, width, label='Tuned Accuracy')
+
+    ax.set_xlabel('Models')
+    ax.set_ylabel('Accuracy')
+    ax.set_title('Accuracy by Classifier and Tuning')
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels, rotation=45)
+    ax.legend()
+
+    plt.tight_layout()
+    plt.show()
